952b5278aa6ae99e7a36dd8024b2e80b47f50d08
QuestDot Software India Pvt Ltd <questdotsoft@gmail.com>
2019-02-18 16:05:03 +0530

    Updates

---
+++ precache-manifest.99722978c21fe2f9f3ed832dca93b018.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abef10c8cf76c2062e82",
+    "url": "/coming-soon-web/static/js/runtime~main.abef10c8.js"
+  },
+  {
+    "revision": "d7b7f5b244e184020d0e",
+    "url": "/coming-soon-web/static/js/main.d7b7f5b2.chunk.js"
+  },
+  {
+    "revision": "d578e8441fb5c1725621",
+    "url": "/coming-soon-web/static/js/2.d578e844.chunk.js"
+  },
+  {
+    "revision": "d7b7f5b244e184020d0e",
+    "url": "/coming-soon-web/static/css/main.d55b605a.chunk.css"
+  },
+  {
+    "revision": "7fa9fea36a629f448c6988040563dd6c",
+    "url": "/coming-soon-web/index.html"
+  }
+];
